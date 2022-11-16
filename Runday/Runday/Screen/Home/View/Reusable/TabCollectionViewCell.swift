38b9f50581b11af1910fff6e1ceb5c4a5a040925
Mini <minnnidev@gmail.com>
2022-11-16 17:29:53 +0900

    [Add] TabCollectionViewCell 추가 #1

---
+++ Runday/Runday/Screen/Home/View/Reusable/TabCollectionViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  TabCollectionViewCell.swift
+//  Runday
+//
+//  Created by 김민 on 2022/11/16.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+// MARK: - TabCollectionViewCell
+
+class TabCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Identifier
+    
+    static let identifier = "TabCollectionViewCell"
+
+    // MARK: - UI Components
+    
+    private let tabLabel = UILabel().then {
+        $0.text = "나의 운동"
+        $0.font = .rundayMedium(ofSize: 16)
+    }
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Custom Method
+    
+    private func setLayout() {
+        
+        backgroundColor = .clear
+        contentView.backgroundColor = .clear
+        
+        contentView.addSubviews(tabLabel)
+        
+        tabLabel.snp.makeConstraints {
+            $0.centerY.centerY.equalToSuperview()
+        }
+    }
+}
