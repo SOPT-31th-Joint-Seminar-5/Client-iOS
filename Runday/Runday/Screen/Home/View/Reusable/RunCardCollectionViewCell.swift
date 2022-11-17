85a78f7973df0e02babf9239fc22268ab634ca4e
Mini <minnnidev@gmail.com>
2022-11-17 16:22:24 +0900

    [Add] RunCardCollectionViewCell 파일 추가 #1

---
+++ Runday/Runday/Screen/Home/View/Reusable/RunCardCollectionViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  RunCardCollectionViewCell.swift
+//  Runday
+//
+//  Created by 김민 on 2022/11/17.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+// MARK: - RunCardCollectionViewCell
+
+class RunCardCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Identifier
+    
+    static let identifier = "RunCardCollectionViewCell"
+    
+    // MARK: - UI Components
+    
+    private let testView = UIView().then {
+        $0.backgroundColor = .yellow
+    }
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Custom Method
+    
+    private func setLayout() {
+        
+        backgroundColor = .clear
+        contentView.backgroundColor = .clear
+        
+        contentView.addSubviews(testView)
+        
+        testView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+}
