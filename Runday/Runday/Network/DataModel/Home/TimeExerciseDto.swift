2780dbcfc0510dfada057b705886474bf95f7357
kpk0616 <kpk0616@naver.com>
2022-11-18 20:45:47 +0900

    [ADD] Home 시간 운동 데이터 모델 추가 #14

---
+++ Runday/Runday/Network/DataModel/Home/TimeExerciseDto.swift
@@ -0,0 +1,32 @@
+//
+//  TimeExerciseDto.swift
+//  Runday
+//
+//  Created by 박의서 on 2022/11/18.
+//
+
+import Foundation
+
+// MARK: - TimeExerciseDto
+struct TimeExerciseDto: Codable {
+    let status: Int
+    let message: String
+    let data: DataClass
+}
+
+// MARK: - DataClass
+struct DataClass: Codable {
+    let user: [User]
+}
+
+// MARK: - User
+struct User: Codable {
+    let id: Int
+    let title, routine, stage: String
+    let isLiked: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case id, title, routine, stage
+        case isLiked = "is_liked"
+    }
+}
