e670487b2a667ec011e8ce3bf0f2e81671af4117
kpk0616 <kpk0616@naver.com>
2022-11-19 15:06:08 +0900

    [FEAT] 네트워크 Like 의 TargetType 구현 #14

---
+++ Runday/Runday/Network/Service/Home/LikeService.swift
@@ -0,0 +1,43 @@
+//
+//  LikeService.swift
+//  Runday
+//
+//  Created by 박의서 on 2022/11/19.
+//
+
+import Moya
+
+enum LikeService {
+    case like(param: LikeRequestDto)
+    case dislike(param: LikeResponseDto)
+}
+
+extension LikeService : BaseTargetType {
+    var path: String {
+        switch self {
+        case .like(param: _):
+            return URLConstant.like
+        case .dislike(param: _):
+            return URLConstant.dislike
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        default: return .put
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .like(param: let param):
+            return .requestParameters(parameters: try! param.asParameter(), encoding: JSONEncoding.default)
+        case .dislike(param: let param):
+            return .requestParameters(parameters: try! param.asParameter(), encoding: JSONEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-Type": "application/json"]
+    }
+}
