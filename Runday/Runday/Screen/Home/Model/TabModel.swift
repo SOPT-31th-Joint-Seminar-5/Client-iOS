98fd42ae1b4d07b644d3ab90ef3ef7c33c1530a2
Mini <minnnidev@gmail.com>
2022-11-22 20:26:49 +0900

    [Add] TabModel 추가 #20

---
+++ Runday/Runday/Screen/Home/Model/TabModel.swift
@@ -0,0 +1,14 @@
+//
+//  TabModel.swift
+//  Runday
+//
+//  Created by 김민 on 2022/11/22.
+//
+
+import Foundation
+
+struct TabModel {
+    let tabName: String
+}
+
+let tabList = ["나의 운동", "레벨 운동", "시간 운동", "맞춤 운동"]
